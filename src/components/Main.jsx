985678775cd94e41452dc128cecb509a4238410e
manyakalra05 <146841072+manyakalra05@users.noreply.github.com>
2025-06-11 19:34:45 +0530

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,19 @@
-export default function Main(props) {
-    const { data } = props
+export default function Main({ data }) {
+  if (data.media_type !== "image") {
     return (
-        <div className="imgContainer">
-            <img src={data.hdurl} alt={data.title || 'bg-img'} className="bgImage" />
-        </div>
-    )
+      <div className="imgContainer">
+        <p>Today's content is not an image.</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="imgContainer">
+      <img
+        src={data.hdurl || data.url}
+        alt={data.title || 'bg-img'}
+        className="bgImage"
+      />
+    </div>
+  );
 }
